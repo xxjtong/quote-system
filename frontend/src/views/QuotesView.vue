04a0651e1db776fc5be9b32f73035df058fb7716
Tong <tong@XiaojunMacBook-Air.local>
2026-05-21 14:37:55 +0800

    fix: 报价管理分页移入卡片内(与产品管理统一)

--- frontend/src/views/QuotesView.vue
+++ frontend/src/views/QuotesView.vue
@@ -303,28 +303,28 @@ function closePreview() {
           </tbody>
         </table>
       </div>
-    </div>
 
-    <!-- Pagination -->
-    <nav v-if="totalPages > 1" class="mt-3">
-      <ul class="pagination pagination-modern justify-content-center mb-0">
-        <li class="page-item" :class="{ disabled: currentPage <= 1 }">
-          <a class="page-link" @click="goPage(1)" title="首页"><i class="bi bi-chevron-double-left"></i></a>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage <= 1 }">
-          <a class="page-link" @click="goPage(currentPage - 1)">上一页</a>
-        </li>
-        <li v-for="p in pageNumbers" :key="p" class="page-item" :class="{ active: p === currentPage }">
-          <a class="page-link" @click="goPage(p)">{{ p }}</a>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage >= totalPages }">
-          <a class="page-link" @click="goPage(currentPage + 1)">下一页</a>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage >= totalPages }">
-          <a class="page-link" @click="goPage(totalPages)" title="末页"><i class="bi bi-chevron-double-right"></i></a>
-        </li>
-      </ul>
-    </nav>
+      <!-- Pagination -->
+      <nav v-if="totalPages > 1" class="mt-3">
+        <ul class="pagination pagination-modern justify-content-center mb-0">
+          <li class="page-item" :class="{ disabled: currentPage <= 1 }">
+            <a class="page-link" @click="goPage(1)" title="首页"><i class="bi bi-chevron-double-left"></i></a>
+          </li>
+          <li class="page-item" :class="{ disabled: currentPage <= 1 }">
+            <a class="page-link" @click="goPage(currentPage - 1)">上一页</a>
+          </li>
+          <li v-for="p in pageNumbers" :key="p" class="page-item" :class="{ active: p === currentPage }">
+            <a class="page-link" @click="goPage(p)">{{ p }}</a>
+          </li>
+          <li class="page-item" :class="{ disabled: currentPage >= totalPages }">
+            <a class="page-link" @click="goPage(currentPage + 1)">下一页</a>
+          </li>
+          <li class="page-item" :class="{ disabled: currentPage >= totalPages }">
+            <a class="page-link" @click="goPage(totalPages)" title="末页"><i class="bi bi-chevron-double-right"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
         <!-- Preview Modal (shared component) -->
     <QuotePreviewModal v-model:show="showPreview" :quote-id="previewQuoteId" :quote-title="previewTitle" />
